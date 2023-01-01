59cf448fef66ac814a2a226785087b0c378526bd
sookyoungwoo <sookyoung2005@gmail.com>
2023-01-01 22:24:15 +0900

    3강

--- fisrt.ts
+++ fisrt.ts
@@ -1,5 +1,4 @@
-let a: string = 'hello';
-a = 1234;
+//기본셋팅
 
 // npm init -y으로 package.json 만들기
 // npm init -y를 입력하면 Study-TS 파일이 node 프로젝트가 됨
@@ -7,4 +6,9 @@ a = 1234;
 // npm i typescript로 타입스크립트 설치
 // 요즘은 npx tsc
 
-//npx tsc --init -> tsconfig.json이 생김
+// npx tsc --init -> tsconfig.json이 생김
+
+// npx tsc --noEmit -> 타입 검사한 내역
+// 이왕이면 editor 아니면 계속 입력해야함
+
+//npx tsc -> ts -> js
