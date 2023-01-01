e15cdbd15d9e12483f3691e74dcd27923d881025
sookyoungwoo <sookyoung2005@gmail.com>
2023-01-01 22:20:11 +0900

    1강 2강

---
+++ fisrt.ts
@@ -0,0 +1,10 @@
+let a: string = 'hello';
+a = 1234;
+
+// npm init -y으로 package.json 만들기
+// npm init -y를 입력하면 Study-TS 파일이 node 프로젝트가 됨
+
+// npm i typescript로 타입스크립트 설치
+// 요즘은 npx tsc
+
+//npx tsc --init -> tsconfig.json이 생김
---
+++ readme.md
@@ -0,0 +1,5 @@
+메인 룰 : typescript는 최종적으로 javascript로 변환된다.
+브라우저는 무조건 자바스크립트만 읽음 :
+tsc컴파일러 : ts -> js로 코드 변환, 타입 검사
+
+node LTS 설치
