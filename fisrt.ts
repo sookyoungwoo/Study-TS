080b9a811832fbc01c795a47a861567c06f41fe0
sookyoungwoo <sookyoung2005@gmail.com>
2023-01-02 22:18:27 +0900

    listen 5 :: 타입스크립트는 변수, 매개변수, 리턴값에 타입 붙이는 것!

--- fisrt.ts
+++ fisrt.ts
@@ -1,14 +1,24 @@
-//기본셋팅
+const a: number = 5;
+const b: string = '5';
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+const f: any = true; // 모든 타입 가능, 아무 의미 없음
+// 형식 선언 소문자여야 함
 
-// npm init -y으로 package.json 만들기
-// npm init -y를 입력하면 Study-TS 파일이 node 프로젝트가 됨
+const h: 5 = 5;
+const i:true = true; // 완전 값 고정
 
-// npm i typescript로 타입스크립트 설치
-// 요즘은 npx tsc
+//function add( x:number, y:number ):number { return x+y } // return 값 타입선언은 매개변수 자리 바로 뒤
+//type Add = (x:number, y:number) => number;
+// type으로 타입을 선언하는 방식 타입 애일리어스(type alias)
+//const add:Add = (x,y) => x+y; //x,y 둘 다 숫자고 return 값도 숫자
 
-// npx tsc --init -> tsconfig.json이 생김
+interface Add{
+    (x:number, y:number): number;
+}
 
-// npx tsc --noEmit -> 타입 검사한 내역
-// 이왕이면 editor 아니면 계속 입력해야함
-
-//npx tsc -> ts -> js
+const arr: string[] = ['123', '456']
+const arr2: number[] = [123,456]
+const arr3: [number, number, string] = [123,456,'hi'] // 개수 지정
+//const obj: { lat:number, lon:number } = { lat: 37.5, lon: 127.5 };
---
+++ setting.ts
@@ -0,0 +1,14 @@
+//기본셋팅
+
+// npm init -y으로 package.json 만들기
+// npm init -y를 입력하면 Study-TS 파일이 node 프로젝트가 됨
+
+// npm i typescript로 타입스크립트 설치
+// 요즘은 npx tsc
+
+// npx tsc --init -> tsconfig.json이 생김
+
+// npx tsc --noEmit -> 타입 검사한 내역
+// 이왕이면 editor 아니면 계속 입력해야함
+
+//npx tsc -> ts -> js
